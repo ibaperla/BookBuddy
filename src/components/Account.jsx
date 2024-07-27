ed223dd2078f94b8a83645c304c9682a152970c4
perla crystal ibarra <perlacrystalibarra@perlas-air.attlocal.net>
2024-07-27 12:20:32 -0700

    did some changes to my project

---
+++ src/components/Account.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+export default function Account ({ SingleBook, Books, }) {
+  const addToCart = (item) => {
+    let resultIndex = 0;
+    let productQty = 0;
+    // check if item is in cart...
+    const result = cart.find((product, index)=>{
+      resultIndex = index;
+      return product.id === item.id;
+    });
+    if (result) {
+      // ...if Yes, then update quantity by 1
+      cart[resultIndex].quantity += 1;
+      productQty = cart[resultIndex].quantity;
+      localStorage.setItem("cart", JSON.stringify(cart));
+      setCart(cart)
+    }else{
+      // if NO, add the item to the cart and set quantity to 1
+
+        item.quantity = 1;
+        productQty = 1;
+        const updatedCart =[...cart, item];
+        localStorage.setItem("cart", JSON.stringify(updatedCart));
+
+        setCart(updatedCart);
+    }
+
+    alert[
+      "Added item! You have " + productQty + " " + item.title + " in your cart!"
+    ];
+  };
+
+  return (
+    <>
+    <Link to={`/product/details/${item?.id}`} className="product-card">
+      <h2>{item?.title}</h2>
+      {parent === "details" ? (
+        <img src={item?.image} alt={item?.title} height={"500px"} />
+      ) : (
+        <img src={item?.image} alt={item?.title} />
+      )}
+      <p>{item?.price?.toFixed(2)}</p>
+      {parent === "details" && <p>{item?.description}</p>}
+      {parent === "details" && token && (
+        <button onClick={()=>addToCart(item)}>Add to Cart </button>
+        )}
+      {parent === "details" && !token && (
+        <></>
+        // <Link to="/login">Login to Add to Cart</Link>
+      )}
+   </Link>
+   </>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+/* TODO - add your code to create a functional 
+React component that renders account details 
+for a logged in user. 
+Fetch the account data from the provided API.
+ You may consider conditionally rendering a 
+ message for other users 
+that prompts them to log in or 
+create an account.  */
---
+++ src/components/Books.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import Details from "./Details";
+import ProductList from "../components/ProductList/ProductList";
+import axios from "axios";
+import "./Home.css";
+
+export default function Home({ 
+  products, 
+  setProducts, 
+  productsToDisplay, 
+  setProductsToDisplay, 
+}) {
+  useEffect(() => {
+    // call api here 
+    axios("https://fakestoreapi.com/products")
+      .then((res) => {
+        setProducts(res.data);
+        setProductsToDisplay(res.data);
+      })
+        .catch((err) => console.log(err));
+  }, []);
+
+  const handleCategoryClick = (category) => {
+    if (category === "") {
+      setProductsToDisplay(products);
+    } else {
+    const filtered = products.filter(
+      (product) => product.category.toLowerCase() === category 
+    );
+    console.log(filtered);
+    setProductsToDisplay(filtered);
+  }
+};
+
+  return (
+    <div>
+      <div className="filter-menu">
+        <span>Filter by Category:</span>
+        <div className="filter-buttons">
+          <p 
+            onClick={() => {
+              handleCategoryClick("men's clothing");
+            }}
+          >
+            Men's Clothing
+          </p>
+          <p 
+            onClick={() => {
+              handleCategoryClick("women's clothing");
+            }}
+          >
+            Women's Clothing
+          </p>
+          <p 
+            onClick={() => {
+              handleCategoryClick("electronics");
+            }}
+          >
+            Electronics
+          </p>
+          <p 
+            onClick={() => {
+              handleCategoryClick("jewelery");
+            }}
+          >
+            Jewelery
+          </p>
+          <p 
+            onClick={() => {
+              handleCategoryClick("");
+            }}
+          >
+            Reset
+          </p>
+        </div>
+      </div>
+      <ProductList products={productsToDisplay} />
+    </div> 
+  );
+}
+
+
+
+/* TODO - add your code to create a functional React
+ component that displays 
+all of the available books in the library's catalog.
+ Fetch the book data from the provided API. Users 
+ should be able to click on an individual book to 
+ navigate to the SingleBook component and view 
+ its details. */
